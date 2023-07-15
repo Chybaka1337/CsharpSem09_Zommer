b7a8aaeb2f070441911accaf48db6df43748db03
Andrey Zommer <zommer.03@gmail.com>
2023-07-15 18:57:48 +0300

    Initial commit

---
+++ Task66/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Task66
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter low bound = ");
+            int.TryParse(Console.ReadLine(), out int lowBound);
+            Console.Write("Enter up bound = ");
+            int.TryParse(Console.ReadLine(), out int upBound);
+            Console.WriteLine(Recursive(lowBound, upBound));
+        }
+
+        static int Recursive(int lowBound, int upBound) 
+        {
+            return upBound == lowBound ? upBound : upBound + Recursive(lowBound, --upBound);
+        }
+    }
+}
---
+++ Task68/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Task68
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(A(3, 2));
+        }
+
+        static uint A(uint n, uint m)
+        {
+            if(n == 0)
+                return m + 1;
+            else 
+                if(n != 0 && m == 0)
+                    return A(n - 1, 1);
+                else
+                    return A(n - 1, A(n, m - 1));
+        }
+    }
+}
